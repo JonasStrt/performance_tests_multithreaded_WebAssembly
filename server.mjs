175f0a36a4ab89a7fdb9601f09572e6881c6c96f
JonasStrt <jonasstratmann@outlook.com>
2024-03-11 13:56:29 +0100

    first commit

---
+++ server.mjs
@@ -0,0 +1,25 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  next();
+});
+
+app.use(express.static(__dirname));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
